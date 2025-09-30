291820c2d10fe70112646c361de83e9f121bce82
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-09-30 09:50:16 +0800

    Rate User and Feedback, and Update User Ratings

---
+++ controller/rateAndFeedback/post.js
@@ -0,0 +1,94 @@
+const { Query } = require("node-appwrite");
+const { database, ID } = require("../../lib/appwrite");
+
+module.exports.RateFeedbackUser = async (req, res) => {
+  try {
+    const { userId, receivedId, rate, feedback } = req.body;
+
+    if (feedback.length > 1549) {
+      return res.status(406).json({
+        success: false,
+        title: "Text reached its limit",
+        message:
+          "Sorry, but we only accept 1550 characters only, please try again.",
+      });
+    }
+
+    const result = await database.createDocument(
+      `${process.env.APPWRITE_DATABASE_ID}`,
+      `${process.env.APPWRITE_RATE_FEED_COLLECTION_ID}`,
+      ID.unique(),
+      {
+        ratedBy: userId,
+        rated: receivedId,
+        rate: Number(rate),
+        feedback,
+      }
+    );
+
+    if (!result) {
+      console.log(result);
+
+      return res.status(400).json({
+        success: false,
+        title: "Error occured",
+        message: "Please try again.",
+      });
+    }
+
+    const averageRate = await database.listDocuments(
+      process.env.APPWRITE_DATABASE_ID,
+      process.env.APPWRITE_RATE_FEED_COLLECTION_ID,
+      [Query.equal("rated", receivedId)]
+    );
+
+    if (averageRate.documents.length > 0) {
+      const average =
+        averageRate.documents.reduce((sum, data) => sum + data.rate, 0) /
+        averageRate.documents.length;
+
+      await database.updateDocument(
+        process.env.APPWRITE_DATABASE_ID,
+        process.env.APPWRITE_USER_COLLECTION_ID,
+        receivedId,
+        { rate: average }
+      );
+    }
+
+    return res.status(200).json({
+      success: true,
+      title: "Rate user successful",
+      message: "You have successfully rated user",
+    });
+  } catch (error) {
+    console.error("Error fetching user:", error);
+
+    if (error.code === 404) {
+      return res.status(404).json({
+        success: false,
+        error: "User Document not found",
+      });
+    }
+
+    if (error.code === 401) {
+      return res.status(401).json({
+        message: "Unathorized user to query this tree",
+      });
+    }
+
+    if (error.code === 400) {
+      return res.status(400).json({
+        success: false,
+        error: "Invalid query parameters",
+        details: error.message,
+      });
+    }
+
+    return res.status(500).json({
+      success: false,
+      error: "Internal server error",
+      details:
+        process.env.NODE_ENV === "development" ? error.message : undefined,
+    });
+  }
+};
--- routing/postRoutes.js
+++ routing/postRoutes.js
@@ -15,31 +15,69 @@ const { saveLeavesToTrees } = require("../controller/leaves/post");
 const { checkUserApiKey } = require("../middleware/checkUserApiKey");
 const { saveTrees } = require("../controller/tree/post");
 const { adminLogin } = require("../controller/admin/post");
+const { RateFeedbackUser } = require("../controller/rateAndFeedback/post");
 
+{
+  /* MESSAGES */
+}
+//Sent A Message
 router.post("/api/v1/users/sent-message", checkUserApiKey, sentMessages);
 
-router.post("/api/v1/users/plots", checkUserApiKey, savePlot);
-
-router.post("/api/v1/users/trees", checkUserApiKey, saveTrees);
-
-router.post("/api/v1/users/product", checkUserApiKey, addProduct);
-
+//Report a user
 router.post("/api/v1/users/report", checkUserApiKey, postReport);
 
+{
+  /* PLOTS */
+}
+//Save Plot
+router.post("/api/v1/users/plots", checkUserApiKey, savePlot);
+
+{
+  /* TREES */
+}
+
+// Save Leaf Image To Tree Collection in Appwrite
 router.post("/api/v1/users/leaves", checkUserApiKey, saveLeavesToTrees);
 
+//Save Tree to Tree Collection
+router.post("/api/v1/users/trees", checkUserApiKey, saveTrees);
+
+{
+  /*Saved A Product */
+}
+//Add A Product
+router.post("/api/v1/users/product", checkUserApiKey, addProduct);
+
+{
+  /* User */
+}
+// Create A New User
 router.post("/api/v1/users", checkRubberTapApiKey, createUser);
 
+//Role Checking Buyer
 router.post("/api/v1/users/role", checkRubberTapApiKey, isBuyer);
 
+//Role Checking Farmer
 router.post("/api/v1/users/farmer", checkRubberTapApiKey, isFarmer);
 
+//Admin Login
+router.post("/api/v1/admin", adminLogin);
+
+{
+  /* Notifications */
+}
+//Weather Rain Alert
 router.post(
   "/api/v1/users/weather-notification",
   checkUserApiKey,
   postNotification
 );
 
-router.post("/api/v1/admin", adminLogin);
+{
+  /* Rate And Feedback */
+}
+
+//Rate User
+router.post("/api/v1/users/rate", checkUserApiKey, RateFeedbackUser);
 
 module.exports = router;
