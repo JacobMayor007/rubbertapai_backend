0600bc7163f094a8b6a3cbc5dee212526b4c7fc5
JacobMayor007 <jacobmary.tapere007@gmail.com>
2025-10-10 12:36:20 +0800

    update user subscription commit

--- controller/payment/post.js
+++ controller/payment/post.js
@@ -1,4 +1,5 @@
-const { database } = require("../../lib/appwrite");
+const dayjs = require("dayjs");
+const { database, ID } = require("../../lib/appwrite");
 
 module.exports.getAllPayments = async (req, res) => {
   try {
@@ -47,9 +48,43 @@ module.exports.subscription = async (req, res) => {
       paymentMethod,
     } = req.params;
 
+    const response = await database.createDocument(
+      `${process.env.APPWRITE_DATABASE_ID}`,
+      `${process.env.APPWRITE_PAY_COLLECTION_ID}`,
+      ID.unique(),
+      {
+        subscriptionType,
+        startDate: dayjs(),
+        endDate: dayjs().add(30, "day"),
+        benefit_1,
+        paymentMethod,
+        price,
+        userId,
+        benefit_2,
+        period,
+      }
+    );
+
+    if (!response) {
+      return res.status(400).json({
+        success: false,
+        title: "Error",
+        message: "There is an eror occured.",
+      });
+    }
+
+    await database.updateDocument(
+      `${process.env.APPWRITE_DATABASE_ID}`,
+      `${process.env.APPWRITE_USER_COLLECTION_ID}`,
+      userId,
+      {
+        subscription: true,
+      }
+    );
+
     return res.status(200).json({
-      title: subscriptionType,
-      message: period,
+      title: "Subscription Success",
+      message: `You have successfully subscribed ${subscriptionType}`,
     });
   } catch (error) {
     console.error("Error fetching products:", error);
